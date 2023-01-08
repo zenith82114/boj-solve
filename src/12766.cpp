ef6d07b0e2c12203ce491d7a0df4401a898b05b9
zenith82114 <zenith82114@gmail.com>
2023-01-08 15:16:19 +0900

    Add 12766

---
+++ src/12766.cpp
@@ -0,0 +1,75 @@
+/*
+ * Q12766 - Dijkstra's alg. + DnC DP optimization
+ * Date: 2023.1.8
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using graph = vector<vector<pair<int, int>>>;
+using i64 = int64_t;
+
+void dijkstra(int s, const graph& g, vector<int>& dist) {
+    priority_queue<pair<i64, int>> pq;
+    pq.emplace(0, s);
+    dist.resize(g.size(), INT_MAX);
+    dist[s] = 0ll;
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop(); d = -d;
+        for (const auto& [v, l] : g[u]) if (dist[v] > d + l) {
+            dist[v] = d + l;
+            pq.emplace(-dist[v], v);
+        }
+    }
+}
+
+vector<i64> pfs, dp0, dp1;
+
+void dnc(int jl, int jr, int kl, int kr) {
+    if (jl > jr) return;
+    int jm = (jl + jr)>>1;
+    i64& ans = dp1[jm] = INT64_MAX;
+    int opt = 0;
+    for (int k = kl; k <= min(jm, kr); ++k) {
+        i64 tmp = dp0[k-1] + (jm - k) * (pfs[jm] - pfs[k-1]);
+        if (ans > tmp) {
+            ans = tmp;
+            opt = k;
+        }
+    }
+    dnc(jl, jm-1, kl, opt);
+    dnc(jm+1, jr, opt, kr);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int N, B, S, R; cin >> N >> B >> S >> R;
+    graph g(N+1), gt(N+1);
+    while (R--) {
+        int u, v, l; cin >> u >> v >> l;
+        g[u].emplace_back(v, l);
+        gt[v].emplace_back(u, l);
+    }
+
+    vector<int> d, dt;
+    dijkstra(B+1, g, d);
+    dijkstra(B+1, gt, dt);
+    pfs.resize(B+1, 0ll);
+    for (int j = 1; j <= B; ++j) pfs[j] = d[j] + dt[j];
+    sort(pfs.begin(), pfs.end());
+    partial_sum(pfs.begin(), pfs.end(), pfs.begin());
+
+    dp0.resize(B+1);
+    dp1.resize(B+1);
+    for (int j = 0; j <= B; ++j) dp1[j] = (j-1) * pfs[j];
+    for (int i = 2; i <= S; ++i) {
+        dp0.swap(dp1);
+        dnc(1, B, 1, B);
+    }
+    cout << dp1[B] << '\n';
+
+    return 0;
+}
