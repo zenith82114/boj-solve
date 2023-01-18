8a27b84125087dca859abfe9b8318c57e64ab95d
zenith82114 <zenith82114@gmail.com>
2023-01-19 01:43:41 +0900

    Add 4360

---
+++ src/4360.cpp
@@ -0,0 +1,73 @@
+/*
+ * Q4360 - Geometric median; Gradient descent
+ * Date: 2023.1.19
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    struct vec2 {
+        double x, y;
+        vec2(double x, double y): x(x), y(y) {}
+        vec2& operator+=(const vec2& v) {
+            x += v.x;
+            y += v.y;
+            return *this;
+        }
+        vec2 operator-(const vec2& v) const {
+            return vec2(x - v.x, y - v.y);
+        }
+        vec2& operator-=(const vec2& v) {
+            x -= v.x;
+            y -= v.y;
+            return *this;
+        }
+        vec2 operator*(const double c) const {
+            return vec2(c * x, c * y);
+        }
+        vec2& operator/=(const double c) {
+            x /= c;
+            y /= c;
+            return *this;
+        }
+        double norm() const { return hypot(x, y); }
+        vec2 normalized() const {
+            double norm = this->norm();
+            return vec2(x / norm, y / norm);
+        }
+    };
+
+    vector<vec2> pts;
+    int N; cin >> N;
+    for (int i = 0; i < N; ++i) {
+        double x, y; cin >> x >> y;
+        pts.emplace_back(x, y);
+    }
+
+    constexpr double eps = 1e-9;
+    vec2 q(5000.0, 5000.0);
+
+    for (int i = 0; i < 2000; ++i) {
+        vec2 grad(0.0, 0.0);
+        for (const auto& p : pts) {
+            vec2 qp = q - p;
+            if (qp.norm() > eps)
+                grad += qp.normalized();
+        }
+        if (grad.norm() < eps) break;
+
+        grad = grad.normalized();
+        double step = i < 1000? 10.0 : 0.01;
+        q -= grad * step;
+    }
+
+    double ans = 0.0;
+    for (const auto& p : pts)
+        ans += (q - p).norm();
+    cout << lrint(ans);
+    return 0;
+}
