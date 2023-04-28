a74327b06c37d392d83c24325d03f4d325705660
zenith82114 <zenith82114@gmail.com>
2023-04-28 17:29:16 +0900

    Rework

--- src/2814.cpp
+++ src/2814.cpp
@@ -1,66 +1,53 @@
 /*
- * Q2814 - Inclusion & Exclusion + Binary search
- * Date: 2023.1.17
- *
- * Warning: slow code (~800ms)
+ * Q2814 - Inclusion/Exclusion + Binary search
+ * Date: 2023.4.28
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(int M, int P, const vector<int>& primes) {
-    int64_t ans = M;
-    auto v = primes;
-    int sgn = -1;
-
-    while (!v.empty()) {
-        for (int& a : v) ans += sgn * (M / a);
-        sgn = -sgn;
-
-        vector<int> nv;
-        for (int& a : v) {
-            for (const int& p : primes) {
-                if ((int64_t)a * p > M) break;
-                if (a % p) nv.emplace_back(a * p);
-            }
-        }
-
-        sort(nv.begin(), nv.end());
-        nv.erase(unique(nv.begin(), nv.end()), nv.end());
-        v.swap(nv);
-    }
-
-    return ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+    cin.tie(0); cout.tie(0);
 
-    constexpr int _1e9 = 1'000'000'000;
     int N, P; cin >> N >> P;
+    if (P > 31607) { cout << (N == 1? P : 0); return 0; }
 
-    if (N == 1) {
-        cout << P; return 0;
-    }
-    if ((int64_t)_1e9 < (int64_t)P * P) {
-        cout << 0; return 0;
-    }
-
+    vector<bool> is_prime(P, true);
     vector<int> primes;
-    bitset<32'000> is_prime; is_prime.set();
     for (int i = 2; i < P; ++i) if (is_prime[i]) {
         primes.emplace_back(i);
-        for (int j = i * i; j < P; j += i) is_prime.reset(j);
+        for (int j = i * i; j < P; j += i) is_prime[j] = false;
     }
 
-    int M = P - 1;
-    int M_end = (_1e9 / P) + 1;
-    for (int k = M_end - M; k; k /= 2)
-        while (M + k <= M_end && solve(M + k, P, primes) < N)
-            M += k;
+    if (P < 50) {
+        const int M = primes.size();
+        auto solve = [&] (int x) {
+            x /= P;
+            int64_t ans = 0;
+            for (int mask = 0; mask < 1<<M; ++mask) {
+                int64_t q = 1;
+                for (int i = 0; i < M && q <= x; ++i)
+                    if ((1<<i) & mask) q *= primes[i];
+                ans += __builtin_popcount(mask) & 1? -(x/q) : x/q;
+            }
+            return ans;
+        };
 
-    int ans = (M + 1) * P;
-    cout << (ans > _1e9? 0 : ans);
+        int l = 1, r = 1e9 + 1;
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (solve(m) < N) l = m+1; else r = m;
+        }
+        cout << (l > 1e9? 0 : l);
+    } else {
+        const int K = 1e9 / P;
+        vector<int> chk(K+1, 1); chk[0] = 0;
+        for (int p : primes) for (int i = p; i <= K; i += p) chk[i] = 0;
+        for (int i = 1; i <= K; ++i)
+            if ((chk[i] += chk[i-1]) == N) { cout << i * P; return 0; }
+        cout << 0;
+    }
+
     return 0;
 }
