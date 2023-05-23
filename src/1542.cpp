0786037e383e6c6834db783692cc09277b19aed7
zenith82114 <zenith82114@gmail.com>
2023-05-23 14:01:02 +0900

    Add 1542

---
+++ src/1542.cpp
@@ -0,0 +1,48 @@
+/*
+ * Q1542 - Sprague-Grundy theorem
+ * Date: 2023.5.23
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int memo[101][101];
+
+int grundy(int x, int y) {
+    int& ret = memo[x][y];
+    if (ret != -1) return ret;
+    set<int> s;
+    for (int k = 0; k < x; ++k) s.emplace(grundy(k, y));
+    for (int k = 0; k < y; ++k) s.emplace(grundy(x, k));
+    for (int k = 1; k < min(x, y); ++k) s.emplace(grundy(x-k, y-k));
+    ret = 0;
+    while (s.find(ret) != s.end()) ++ret;
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    using ii = pair<int, int>;
+
+    for (int x = 0; x <= 100; ++x)
+    for (int y = 0; y <= 100; ++y)
+        memo[x][y] = (!x || !y || x == y)? -2 : -1;
+
+    int TC = 5;
+    while (TC--) {
+        int N; cin >> N;
+        vector<ii> v(N);
+        for (auto& [x, y] : v) cin >> x >> y;
+        if (any_of(v.begin(), v.end(), [] (const ii& a) {
+            const auto& [x, y] = a; return !x || !y || x == y;
+        })) {
+            cout << "S\n";
+        } else {
+            int g = 0;
+            for (auto& [x, y] : v) g ^= grundy(x, y);
+            cout << (g? "S\n" : "D\n");
+        }
+    }
+
+    return 0;
+}
