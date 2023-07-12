aa0e4478b83d5a58ab0d2c913aa52a5f58b5f05c
zenith82114 <zenith82114@gmail.com>
2023-07-12 19:43:29 +0900

    bugfix

--- src/17613.cpp
+++ src/17613.cpp
@@ -1,40 +1,34 @@
 /*
- * Q17613 - Greedy + DnC
- * Date: 2022.12.27
+ * Q17613 - DnC
+ * Date: 2023.7.12
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> v;
+int v[32];
 
 int dnc(int x, int y) {
     if (!x && !y) return 0;
 
     int i = 0;
-    for (; i+1 < 32 && v[i+1] <= x; ++i);
+    for (; i+1 < 31 && v[i+1] <= x; ++i);
     if (y < v[i+1])
         return dnc(x - v[i], y - v[i]) + i;
 
     int j = i;
-    for (; j+1 < 32 && v[j+1] <= y; ++j);
+    for (; j+1 < 31 && v[j+1] <= y; ++j);
 
-    int ret = 0;
-    if (v[j+1] == y+1)
-        ret = 1 + (j*(j+1))/2;
-    else if (i+1 < y)
-        ret = 1 + ((j-1)*j)/2;
-    else if (v[i] == x)
-        ret = 1 + (i*(i+1))/2;
-    return max({ret, dnc(x, v[i+1] -1), dnc(v[j], y)});
+    if (i+1 < j)
+        return max(1 + ((j-1)*j)/2, dnc(v[j], y));
+    else
+        return max(dnc(x, v[j] - 1), dnc(v[j], y));
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+    ios_base::sync_with_stdio(false); cin.tie(0);
 
-    for (int i = 0; i < 32; ++i)
-        v.emplace_back((1<<i) -1);
+    for (int i = 0; i < 31; ++i) v[i] = (1<<i) -1;
 
     int T; cin >> T;
     while (T--) {
