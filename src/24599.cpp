1f987b5f948116ab9140af2821933900667e09b9
zenith82114 <zenith82114@gmail.com>
2023-08-01 21:06:42 +0900

    Rework

--- src/24599.cpp
+++ src/24599.cpp
@@ -1,79 +1,47 @@
 /*
- * Q24599 - Greedy + 2-SAT
- * Date: 2023.4.4
+ * Q24599 - Greedy + Floyd-Warshall
+ * Date: 2023.8.1
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int clk = 0;
-int scc_id = 0;
-int entry[200], link[200], scc[200];
-vector<int> adj[200];
-stack<int> st;
-bool on_st[200];
-
-void dfs(int u) {
-    entry[u] = link[u] = ++clk;
-    st.emplace(u); on_st[u] = true;
-    for (const int& v : adj[u]) {
-        if (!entry[v]) {
-            dfs(v);
-            link[u] = min(link[u], link[v]);
-        }
-        else if (on_st[v])
-            link[u] = min(link[u], entry[v]);
-    }
-    if (entry[u] == link[u]) {
-        ++scc_id;
-        while (st.top() != u) {
-            int v = st.top();
-            st.pop(); on_st[v] = false;
-            scc[v] = scc_id;
-        }
-        st.pop(); on_st[u] = false;
-        scc[u] = scc_id;
-    }
-}
+bool path[200][200] {};
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false); cin.tie(0);
 
     int N, K; cin >> N >> K;
+    const int V = K<<1;
     while (N--) {
         string s; cin >> s;
-        for (int i = 0;   i < K; ++i) if (s[i] != 'X')
-        for (int j = i+1; j < K; ++j) if (s[j] != 'X') {
-            int ui = i<<1 ^ (s[i] == 'F');
-            int uj = j<<1 ^ (s[j] == 'F');
-            adj[ui].emplace_back(uj ^ 1);
-            adj[uj].emplace_back(ui ^ 1);
+        for (int i = 0; i < K; ++i) if (s[i] != 'X') {
+            int xi = s[i] == 'F'? i+K : i;
+            for (int j = 0; j < K; ++j) if (i != j && s[j] != 'X') {
+                int xj = s[j] == 'F'? j : j+K;
+                path[xi][xj] = true;
+            }
         }
     }
 
-    const auto test = [&] (void) {
-        clk = 0;
-        scc_id = 0;
-        memset(entry, 0, sizeof entry);
-        memset(link, 0, sizeof link);
-        memset(scc, 0, sizeof scc);
-        for (int i = 0; i < 2*K; ++i) if (!scc[i]) dfs(i);
-        for (int i = 0; i < K; ++i)
-            if (scc[i<<1] == scc[i<<1|1]) return false;
-        return true;
-    };
+    for (int k = 0; k < V; ++k)
+    for (int i = 0; i < V; ++i)
+    for (int j = 0; j < V; ++j)
+        path[i][j] = path[i][j] || (path[i][k] && path[k][j]);
 
-    if (!test()) { cout << -1; return 0; }
+    for (int i = 0; i < K; ++i) if (path[i][i+K] && path[i+K][i]) {
+        cout << -1; return 0;
+    }
 
-    for (int i = 0; i < K; ++i) {
-        int t = i<<1, f = t^1;
-        adj[t].emplace_back(f);
-        if (test()) cout << 'F';
+    for (int t = 0; t < K; ++t) {
+        if (!path[t][t+K] && path[t+K][t]) cout << 'T'; else
+        if (!path[t+K][t] && path[t][t+K]) cout << 'F';
         else {
-            adj[t].pop_back();
-            adj[f].emplace_back(t);
-            cout << 'T';
+            cout << 'F';
+            path[t][t+K] = true;
+            for (int i = 0; i < V; ++i)
+            for (int j = 0; j < V; ++j)
+                path[i][j] = path[i][j] || (path[i][t] && path[t+K][j]);
         }
     }
 
