b7325f513de55b64cb7aba2599e221bac07a85b0
zenith82114 <zenith82114@gmail.com>
2023-08-22 11:10:30 +0900

    polish

--- src/11400.cpp
+++ src/11400.cpp
@@ -1,29 +1,26 @@
 /*
  * Q11400 - Bridges
- * Date: 2022.8.29
+ * Date: 2023.8.22
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int> > graph;
-vector<int> disc, early, parent;
+vector<int> graph[100'001];
+int disc[100'001], early[100'001];
 vector<pair<int, int> > bridges;
 int clk = 0;
 
-void dfs(int u) {
-    int child = 0;
-    disc[u] = early[u] = clk++;
+void dfs(int pu, int u) {
+    disc[u] = early[u] = ++clk;
     for (int& v : graph[u]) {
-        if (disc[v] == -1) {
-            ++child;
-            parent[v] = u;
-            dfs(v);
+        if (!disc[v]) {
+            dfs(u, v);
             early[u] = min(early[u], early[v]);
             if (disc[u] < early[v])
                 bridges.emplace_back(min(u, v), max(u, v));
         }
-        else if (parent[u] != v)
+        else if (v != pu)
             early[u] = min(early[u], disc[v]);
     }
 }
@@ -33,23 +30,16 @@ int main() {
     cin.tie(nullptr); cout.tie(nullptr);
 
     int V, E; cin >> V >> E;
-    graph.resize(V+1);
-    bridges.reserve(E);
     while (E--) {
         int u, v; cin >> u >> v;
         graph[u].emplace_back(v);
         graph[v].emplace_back(u);
     }
 
-    disc    .resize(V+1, -1);
-    early   .resize(V+1);
-    parent  .resize(V+1, 0);
-    for (int u = 1; u <= V; ++u)
-        if (disc[u] == -1) dfs(u);
+    for (int u = 1; u <= V; ++u) if (!disc[u]) dfs(0, u);
 
     cout << bridges.size() << '\n';
     sort(bridges.begin(), bridges.end());
-    for (const auto& e : bridges)
-        cout << e.first << ' ' << e.second << '\n';
+    for (const auto& [u, v] : bridges) cout << u << ' ' << v << '\n';
     return 0;
 }
