7491480d2b632fbeb8aa6c6c9eff512bbeef4c13
zenith82114 <zenith82114@gmail.com>
2023-09-02 00:12:52 +0900

    Rework

--- src/2206.cpp
+++ src/2206.cpp
@@ -1,74 +1,49 @@
 /*
- * Q2206 - Grid maze BFS
- * Date: 2021.7.15
+ * Q2206 - BFS
+ * Date: 2023.9.2
  */
 
-#include <iostream>
-#include <queue>
-#include <tuple>
+#include<bits/stdc++.h>
 using namespace std;
-constexpr int INF = INT32_MAX;
 
-int N, M;
-bool A[1001][1001];
-int B[1001][1001], C[1001][1001];
-int dm[4] = { 1,0,-1,0 };
-int dn[4] = { 0,-1,0,1 };
-
-inline bool IN_RANGE(int n, int m) {
-    return (n > 0 && n <= N && m > 0 && m <= M);
-}
+constexpr int inf = INT32_MAX;
+constexpr int dx[] = { 1, -1, 0, 0 };
+constexpr int dy[] = { 0, 0, 1, -1 };
+char b[1000][1000];
+int dist[1000][1000][2] {};
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+    ios_base::sync_with_stdio(false); cin.tie(0);
 
-    cin >> N >> M;
-    for (int n = 1; n <= N; ++n) {
-        for (int m = 1; m <= M; ++m) {
-            char t; cin >> t;
-            // data (map)
-            A[n][m] = t - '0';
-            // "footprint" array
-            // -1: not reached
-            // 0: reached using wallbreak
-            // 1: reached without using wallbreak
-            B[n][m] = -1;
-            // cost array - least steps known so far needed to reach [n][m]
-            C[n][m] = INF;
-        }
+    int N, M; cin >> N >> M;
+    for (int x = 0; x < N; ++x)
+    for (int y = 0; y < M; ++y) {
+        cin >> b[x][y];
+        dist[x][y][0] = dist[x][y][1] = inf;
     }
 
-    queue<tuple<int, int, int, int> > Q;
-    Q.emplace(1, 1, 1, 1);
-    while (!Q.empty()) {
-        auto [n, m, b, c] = Q.front(); Q.pop();
-        // When we should update a cell:
-        // 1. we find a "better" state for it (B value increases)
-        // 2. we find a "shorter" path to it (C value decreases with same B value)
-        if (B[n][m] < b || (B[n][m] == b && C[n][m] > c)) {
-            B[n][m] = b;
-            C[n][m] = c;
-            if (n == N && m == M)
-                break;
-            for (int k = 0; k < 4; ++k) {
-                int n_ = n + dn[k];
-                int m_ = m + dm[k];
-                if (IN_RANGE(n_, m_) &&
-                    (B[n_][m_] < b ||
-                    (B[n_][m_] == b && C[n_][m_] > c + 1))) {
-                    if (!A[n_][m_])
-                        Q.emplace(n_, m_, b, c + 1);
-                    // use wallbreak if not used yet
-                    else if (b)
-                        Q.emplace(n_, m_, 0, c + 1);
-                }
+    queue<tuple<int, int, int> > q;
+    q.emplace(0, 0, 0);
+    dist[0][0][0] = 1;
+
+    while (!q.empty()) {
+        auto [x, y, k] = q.front(); q.pop();
+        for (int t = 0; t < 4; ++t) {
+            int nx = x + dx[t];
+            int ny = y + dy[t];
+            if (nx == -1 || nx == N || ny == -1 || ny == M) continue;
+            if (b[nx][ny] == '0' && dist[nx][ny][k] > dist[x][y][k] + 1) {
+                dist[nx][ny][k] = dist[x][y][k] + 1;
+                q.emplace(nx, ny, k);
+            }
+            else if (b[nx][ny] == '1' && !k && dist[nx][ny][1] > dist[x][y][0] + 1) {
+                dist[nx][ny][1] = dist[x][y][0] + 1;
+                q.emplace(nx, ny, 1);
             }
         }
     }
-    if (C[N][M] == INF) cout << -1;
-    else cout << C[N][M];
-    cout << '\n';
 
+    int ans = min(dist[N-1][M-1][0], dist[N-1][M-1][1]);
+    cout << (ans == inf? -1 : ans);
     return 0;
 }
