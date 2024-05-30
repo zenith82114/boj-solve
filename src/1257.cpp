fe8de44bc179d580d15686a1dc5d774b1f85e5c5
zenith82114 <zenith82114@gmail.com>
2024-05-30 12:41:07 +0900

    Polish

--- src/1257.cpp
+++ src/1257.cpp
@@ -1,16 +1,14 @@
 /*
  * Q1257 - 0-1 BFS
- * Date: 2023.5.7
+ * Date: 2024.5.30
  */
 
 #include<bits/stdc++.h>
 using namespace std;
+using i64 = int64_t;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    using i64 = int64_t;
-    using ii = pair<int, int>;
+    cin.tie(0)->sync_with_stdio(0);
 
     i64 M; cin >> M;
     int N; cin >> N;
@@ -18,21 +16,24 @@ int main() {
     int P = 0;
     for (int& a : A) cin >> a, P = max(P, a);
 
-    vector<vector<ii> > adj(P);
-    for (int x = 0; x < P; ++x) for (int a : A) {
-        if (x + a < P) adj[x].emplace_back(x + a, 1);
-        else adj[x].emplace_back(x + a - P, 0);
-    }
     vector<int> dist(P, P); dist[0] = 0;
     deque<int> dq; dq.emplace_front(0);
     while (!dq.empty()) {
-        auto x = dq.front(); dq.pop_front();
-        for (auto& [y, w] : adj[x]) if (dist[y] > dist[x] + w) {
-            dist[y] = dist[x] + w;
-            if (w & 1) dq.emplace_back(y);
-            else dq.emplace_front(y);
+        int x = dq.front(); dq.pop_front();
+        for (int a : A) {
+            int y = x + a;
+            if (y < P) {
+                if (dist[y] > dist[x] + 1) {
+                    dist[y] = dist[x] + 1;
+                    dq.emplace_back(y);
+                }
+            }
+            else if (dist[y - P] > dist[x]) {
+                dist[y - P] = dist[x];
+                dq.emplace_front(y - P);
+            }
         }
     }
-    cout << dist[M % P] + (M / P);
+    cout << (M / P) + dist[M % P];
     return 0;
 }
