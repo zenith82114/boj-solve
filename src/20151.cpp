94dc10d3b991d7ea807de8a29819693c645a8bbd
zenith82114 <zenith82114@gmail.com>
2024-11-17 13:21:05 +0900

    Rejudge defense

--- src/20151.cpp
+++ src/20151.cpp
@@ -1,6 +1,6 @@
 /*
  * Q20151 - Shamos-Hoey alg.
- * Date: 2023.6.8
+ * Date: 2024.11.17
  */
 
 #include<bits/stdc++.h>
@@ -14,21 +14,21 @@ struct point {
         return make_pair(x, y) < make_pair(p.x, p.y);
     }
     bool operator==(const point& p) const {
-        return make_pair(x, y) == make_pair(p.x, p.y);
+        return x == p.x && y == p.y;
     }
 };
 
 struct segment {
     point l, r;
     int id;
-    long double eval(i64 x0) const {
-        return l.y + (long double)(r.y - l.y) * (x0 - l.x) / (r.x - l.x);
+    long double eval(i64 x) const {
+        return l.y + (r.y - l.y) * ((long double)(x - l.x) / (r.x - l.x));
     }
     bool operator<(const segment& seg) const {
-        i64 x0 = max(l.x, seg.l.x);
-        if (abs(eval(x0) - seg.eval(x0)) > 1e-10L)
-            return eval(x0) < seg.eval(x0);
-        return id < seg.id;
+        /* assume enough difference on at least one side (left or right) */
+        i64 x = max(l.x, seg.l.x);
+        if (l == seg.l) x = min(r.x, seg.r.x);
+        return 1e-18l < seg.eval(x) - eval(x);
     }
 };
 
@@ -43,10 +43,12 @@ bool intersect(const segment& s1, const segment& s2) {
     const auto& [a, b, i1] = s1;
     const auto& [c, d, i2] = s2;
 
+    /* two endpoints touching */
     if (a == c && ccw(a, b, d) != 0) return false;
     if (b == d && ccw(a, b, c) != 0) return false;
     if (a == d || b == c) return false;
 
+    /* general */
     int ab = ccw(a, b, c) * ccw(a, b, d);
     int cd = ccw(c, d, a) * ccw(c, d, b);
     if (!(ab || cd)) return !(b < c || d < a);
@@ -54,8 +56,8 @@ bool intersect(const segment& s1, const segment& s2) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    constexpr i64 K = 2e9 + 1;
+    cin.tie(0)->sync_with_stdio(0);
+    constexpr i64 K = 2'000'000'001;
 
     int N; cin >> N;
 
@@ -63,9 +65,11 @@ int main() {
     for (int i = 0; i < N; ++i) {
         auto& [l, r, id] = segm[i];
         i64 x, y;
+        /* remove vertical segments via rotation */
         cin >> x >> y; l.x = K*x - y; l.y = x + K*y;
         cin >> x >> y; r.x = K*x - y; r.y = x + K*y;
         if (r < l) swap(l, r);
+        assert(l.x < r.x);
         id = i;
     }
 
@@ -78,31 +82,32 @@ int main() {
     sort(evt.begin(), evt.end(), [&] (const ii& e1, const ii& e2) {
         const auto& [i1, f1] = e1;
         const auto& [i2, f2] = e2;
-        const point& p1 = (f1 == 1? segm[i1].l : segm[i1].r);
-        const point& p2 = (f2 == 1? segm[i2].l : segm[i2].r);
-        return make_tuple(p1.x, f1, p1.y) < make_tuple(p2.x, f2, p2.y);
+        const point& p1 = (f1 == 0? segm[i1].l : segm[i1].r);
+        const point& p2 = (f2 == 0? segm[i2].l : segm[i2].r);
+        /* right endpoints (deletions) go first */
+        return make_tuple(p1.x, -f1, p1.y) < make_tuple(p2.x, -f2, p2.y);
     });
 
-    multiset<segment> ms;
-    vector<multiset<segment>::iterator> ptrs(N);
+    set<segment> s;
+    vector<set<segment>::iterator> iters(N);
 
     for (const auto& [i, f] : evt) {
-        if (f == 1) {
-            auto it = ms.emplace(segm[i]);
-            if (it != ms.begin() && intersect(segm[i], *prev(it))) {
+        if (f == 0) {
+            auto p = s.lower_bound(segm[i]);
+            if (p != s.end() && intersect(*p, segm[i])) {
                 cout << 1; return 0;
             }
-            if (next(it) != ms.end() && intersect(segm[i], *next(it))) {
+            if (p != s.begin() && intersect(*prev(p), segm[i])) {
                 cout << 1; return 0;
             }
-            ptrs[i] = it;
+            iters[i] = s.insert(p, segm[i]);
         }
         else {
-            auto it = ptrs[i];
-            if (it != ms.begin() && next(it) != ms.end() && intersect(*prev(it), *next(it))) {
+            auto p = iters[i];
+            if (next(p) != s.end() && p != s.begin() && intersect(*next(p), *prev(p))) {
                 cout << 1; return 0;
             }
-            ms.erase(it);
+            s.erase(p);
         }
     }
     cout << 0; return 0;
