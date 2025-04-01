24cb4b7ad9f21a30d19240d2dbc9034bdff97e71
zenith82114 <zenith82114@gmail.com>
2025-04-01 21:29:42 +0900

    Polish

--- src/10538.cpp
+++ src/10538.cpp
@@ -1,6 +1,6 @@
 /*
  * Q10538 - Aho-Corasick + KMP
- * Date: 2023.8.14
+ * Date: 2025.4.1
  */
 
 #include<bits/stdc++.h>
@@ -13,14 +13,18 @@ int ff[2000] {};
 class Trie {
     struct Node {
         Node *chld[2];
-        Node *prnt, *sufx;
+        Node *prnt, *fail;
         int chr, pid;
-        Node(int c): chld(), prnt(0), sufx(0), chr(c), pid(-1) {}
+        Node(int c): chld(), prnt(0), fail(0), chr(c), pid(-1) {}
     };
     Node *root;
+    int count;
 public:
-    void init() { root = new Node(-1); }
-    void insert(const string& pattern, int pid) {
+    void init() {
+        root = new Node(-1);
+        count = 0;
+    }
+    int insert(const string& pattern) {
         Node *cur = root;
         for (const char& p : pattern) {
             int chr = p == 'o'? 0 : 1;
@@ -31,12 +35,13 @@ public:
             }
             cur = nxt;
         }
-        cur->pid = pid;
+        if (cur->pid == -1) cur->pid = ++count;
+        return cur->pid;
     }
     void build() {
         queue<Node *> q;
         for (auto tmp : root->chld) if (tmp) {
-            tmp->sufx = root;
+            tmp->fail = root;
             q.emplace(tmp);
         }
         while (!q.empty()) {
@@ -45,11 +50,11 @@ public:
             if (cur->chld[1]) q.emplace(cur->chld[1]);
 
             Node *tmp = cur->prnt;
-            while (tmp != root && !cur->sufx) {
-                tmp = tmp->sufx;
-                cur->sufx = tmp->chld[cur->chr];
+            while (tmp != root && !cur->fail) {
+                tmp = tmp->fail;
+                cur->fail = tmp->chld[cur->chr];
             }
-            if (!cur->sufx) cur->sufx = root;
+            if (!cur->fail) cur->fail = root;
         }
     }
     vector<pair<int, int> > search(const string& text) {
@@ -58,7 +63,7 @@ public:
 
         for (int i = 0; i < (int)text.size(); ++i) {
             int chr = text[i] == 'o'? 0 : 1;
-            while (cur != root && !cur->chld[chr]) cur = cur->sufx;
+            while (cur != root && !cur->chld[chr]) cur = cur->fail;
             if (cur->chld[chr]) cur = cur->chld[chr];
             if (cur->pid != -1) matches.emplace_back(i, cur->pid);
         }
@@ -70,13 +75,12 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
 
     int hp, wp, hm, wm; cin >> hp >> wp >> hm >> wm;
-    vector<string> v(hp); for (auto& p : v) cin >> p;
 
-    auto sv(v); sv.emplace_back();
-    sort(sv.begin(), sv.end());
-    sv.erase(unique(sv.begin(), sv.end()), sv.end());
     trie.init();
-    for (int i = 1; i < (int)sv.size(); ++i) trie.insert(sv[i], i);
+    for (int i = 0; i < hp; ++i) {
+        string p; cin >> p;
+        b[i] = trie.insert(p);
+    }
     trie.build();
 
     for (int i = 0; i < hm; ++i) {
@@ -85,21 +89,18 @@ int main() {
         for (const auto& [j, pid] : matches) a[i][j] = pid;
     }
 
-    for (int i = 0; i < hp; ++i) {
-        b[i] = lower_bound(sv.begin(), sv.end(), v[i]) - sv.begin();
-    }
     for (int i = 1, j = 0; i < hp; ++i) {
-        while (j && b[i] != b[j]) j = ff[j-1];
+        while (j && b[i] != b[j]) j = ff[j - 1];
         if (b[i] == b[j]) ++j;
         ff[i] = j;
     }
 
     int ans = 0;
-    for (int col = wp-1; col < wm; ++col) {
+    for (int col = wp - 1; col < wm; ++col) {
         for (int i = 0, j = 0; i < hm; ++i) {
-            while (j && a[i][col] != b[j]) j = ff[j-1];
+            while (j && a[i][col] != b[j]) j = ff[j - 1];
             if (a[i][col] == b[j]) ++j;
-            if (j == hp) ++ans, j = ff[j-1];
+            if (j == hp) ++ans, j = ff[j - 1];
         }
     }
 
