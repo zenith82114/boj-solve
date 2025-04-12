94f54082d99baad7df603804ece2d94fcab1bb88
zenith82114 <zenith82114@gmail.com>
2025-04-12 18:48:49 +0900

    Polish

--- src/9662.cpp
+++ src/9662.cpp
@@ -1,13 +1,13 @@
 /*
- * Q9662 - Bitmask + Floyd's tortoise and hare
- * Date: 2023.6.19
+ * Q9662 - bitmask, Floyd's tortoise and hare
+ * Date: 2025.4.12
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
     using u32 = uint32_t;
 
     int M; cin >> M;
@@ -29,14 +29,10 @@ int main() {
 
     u32 x0 = 0u;
     for (int i = 1; i < bw; ++i) x0 |= 1u<<i;
-    x0 = f(x0);
 
     u32 x = f(x0);
-    u32 y = f(x);
-    while (x != y) {
-        x = f(x);
-        y = f(f(y));
-    }
+    u32 y = f(f(x0));
+    while (x != y) x = f(x), y = f(f(y));
     x = x0;
     int q = 0;
     while (x != y) x = f(x), y = f(y), q++;
@@ -47,25 +43,25 @@ int main() {
     if (M < q) {
         x = x0;
         for (int i = 0; i < M; ++i) {
-            if (~x & 1u) ans++;
             x = f(x);
+            if (~x & 1u) ans++;
         }
     }
     else {
         M -= q;
         for (int i = q; i < q + p; ++i) {
-            if (~x & 1u) ans++;
             x = f(x);
+            if (~x & 1u) ans++;
         }
         ans *= (M / p);
         for (int i = q; i < q + (M % p); ++i) {
-            if (~x & 1u) ans++;
             x = f(x);
+            if (~x & 1u) ans++;
         }
         x = x0;
         for (int i = 0; i < q; ++i) {
-            if (~x & 1u) ans++;
             x = f(x);
+            if (~x & 1u) ans++;
         }
     }
     cout << ans;
