f629d4a1360f833620a57c10009cb0101a2ae44a
zenith82114 <zenith82114@gmail.com>
2025-06-28 16:57:51 +0900

    Rework

--- src/3391.cpp
+++ src/3391.cpp
@@ -1,85 +1,85 @@
 /*
- * Q3391 - Konig's theorem + Hopcroft-Karp alg.
- * Date: 2023.7.31
+ * Q3391 - Konig's theorem, Dinic's alg.
+ * Date: 2025.6.28
  */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-constexpr int inf = INT32_MAX;
 const int dx[] { -2,-2,-1,-1,1,1,2,2 };
 const int dy[] { -1,1,-2,2,-2,2,-1,1 };
 
-int N, nil;
+int n, s, t;
 bool flag[200][200];
-vector<int> adj[40'000];
-vector<int> U;
-int level[40'001] {};
-int match[40'000] {};
+vector<pair<int, int> > adj[40'002];
+vector<int> cap;
+int level[40'002] {};
+unsigned hist[40'002] {};
+
+void add_edge(int u, int v) {
+    adj[u].emplace_back(v, cap.size());
+    adj[v].emplace_back(u, cap.size() + 1);
+    cap.emplace_back(1);
+    cap.emplace_back(0);
+}
 
 bool bfs() {
-    queue<int> q;
-    for (int u : U) {
-        if (match[u] == nil) {
-            level[u] = 0;
-            q.emplace(u);
-        } else level[u] = inf;
-    }
-    level[nil] = inf;
+    queue<int> q; q.emplace(s);
+    fill(level, level + s, -1);
+    level[s] = 0; level[t] = -1;
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        if (u == nil) break;
-        for (int v : adj[u]) {
-            if (level[match[v]] == inf) {
-                level[match[v]] = level[u] + 1;
-                q.emplace(match[v]);
+        for (const auto& [v, e] : adj[u]) {
+            if (level[v] == -1 && cap[e]) {
+                level[v] = level[u] + 1;
+                q.emplace(v);
             }
         }
     }
-    return level[nil] != inf;
+    return level[t] != -1;
 }
 
 int dfs(int u) {
-    if (u == nil) return 1;
-    for (int v : adj[u]) if (level[match[v]] == level[u] + 1 && dfs(match[v])) {
-        match[v] = u;
-        match[u] = v;
-        return 1;
+    if (u == t) return 1;
+    for (auto& i = hist[u]; i < adj[u].size(); ++i) {
+        const auto& [v, e] = adj[u][i];
+        if (level[v] == level[u] + 1 && cap[e] && dfs(v)) {
+            cap[e] = 0;
+            cap[e ^ 1] = 1;
+            return 1;
+        }
     }
-    level[u] = inf;
     return 0;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int M; cin >> N >> M;
-    for (int i = 0; i < M; ++i) {
+    int m; cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
         int x, y; cin >> x >> y;
         flag[--x][--y] = true;
     }
-    nil = N*N;
+    s = n*n; t = s + 1;
 
-    for (int x = 0; x < N; ++x)
-    for (int y = 0; y < N; ++y) {
-        if (flag[x][y]) continue;
-        int id = x*N + y;
-        match[id] = nil;
-
+    for (int x = 0; x < n; ++x)
+    for (int y = 0; y < n; ++y) if (!flag[x][y]) {
+        int id = x*n + y;
+        if ((x ^ y) & 1) add_edge(s, id); else add_edge(id, t);
         for (int k = 0; k < 8; ++k) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if (flag[nx][ny]) continue;
-            adj[id].emplace_back(nx*N + ny);
+            int xx = x + dx[k];
+            int yy = y + dy[k];
+            if (xx < 0 || xx >= n || yy < 0 || yy >= n) continue;
+            if (flag[xx][yy]) continue;
+            if ((x ^ y) & 1) add_edge(id, xx*n + yy);
+            else add_edge(xx*n + yy, id);
         }
-        if ((x^y)&1) U.emplace_back(id);
-        else adj[id].emplace_back(nil);
     }
 
-    int ans = N*N - M;
+    int ans = n*n - m;
     while (bfs()) {
-        for (int u : U) if (match[u] == nil) ans -= dfs(u);
+        fill(hist, hist + t + 1, 0);
+        while (dfs(s)) --ans;
     }
     cout << ans;
     return 0;
