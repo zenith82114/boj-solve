4e4c271ab252a28aaa03c64824249f13a9e1153c
zenith82114 <zenith82114@gmail.com>
2025-07-18 13:46:14 +0900

    Rework

--- src/13309.cpp
+++ src/13309.cpp
@@ -1,107 +1,87 @@
 /*
- * Q13309 - Heavy-light decomposition
- * Date: 2022.12.22
+ * Q13309 - Euler tour, LCA, segment tree
+ * Date: 2025.7.18
  */
 
 #include<bits/stdc++.h>
 using namespace std;
-
-struct HLD {
-    static const int ROOT = 1;
-    vector<int> par, sz;
-    vector<int> ch_id, ch_dep, ch_pos;
-    vector<int> et;
-    void dfs(int pu, int u, const vector<vector<int>>& tree) {
-        par[u] = pu;
-        sz[u] = 1;
-        for (const int& v : tree[u]) if (v != pu) {
-            dfs(u, v, tree);
-            sz[u] += sz[v];
-        }
-    }
-    void run(int u, int ci, int cd, int cp, int& e, const vector<vector<int>>& tree) {
-        ch_id[u] = ci;
-        ch_dep[u] = cd;
-        ch_pos[u] = cp;
-        et[u] = e++;
-
-        int hv = 0;
-        for (const int& v : tree[u]) if (v != par[u] && sz[hv] < sz[v])
-            hv = v;
-        if (hv != 0)
-            run(hv, ci, cd, cp+1, e, tree);
-        for (const int& v : tree[u]) if (v != par[u] && v != hv)
-            run(v, v, cd+1, 0, e, tree);
-    }
-    HLD(const vector<vector<int>>& tree) {
-        int N = tree.size();
-        par.resize(N);
-        sz. resize(N);
-        ch_id .resize(N);
-        ch_dep.resize(N);
-        ch_pos.resize(N);
-        et.resize(N);
-
-        dfs(0, ROOT, tree); // sz[0] == 0
-        int e = 0;
-        run(ROOT, 1, 0, 0, e, tree);
-    }
-};
+using i64 = int64_t;
+constexpr int MAXN = 2e5 + 4, MAXLOGN = 18;
 
 class SegTree {
-    int N;
-    vector<bool> arr;
+    vector<int> tr;
+    int n;
 public:
-    SegTree(int n) {
-        for (N = 1; N < n; N <<= 1);
-        arr.resize(N<<1, true);
+    SegTree(int sz) {
+        n = 1; while (n < sz) n *= 2;
+        tr.resize(2*n);
     }
-    void update(int i) {
-        i |= N;
-        arr[i] = false;
-        for (; i > 1; i >>= 1)
-            arr[i>>1] = arr[i] && arr[i^1];
+    void upd(int i, int x) {
+        tr[i |= n] = x;
+        for (; i > 1; i /= 2) tr[i/2] = tr[i] + tr[i^1];
     }
-    bool query(int i, int j) {
-        bool ans = true;
-        for (i |= N, j |= N; i <= j; i >>= 1, j >>= 1) {
-            if (i&1)    { ans = ans && arr[i]; ++i; }
-            if ((~j)&1) { ans = ans && arr[j]; --j; }
+    int qry(int j) {
+        int i = n; j |= n;
+        int ans = 0;
+        for (; i <= j; i /= 2, j /= 2) {
+            if ( i&1) ans += tr[i++];
+            if (~j&1) ans += tr[j--];
         }
         return ans;
     }
 };
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
+vector<int> g[MAXN];
+int rmq[MAXLOGN + 1][2 * MAXN];
+array<int, MAXN> dep, ts, te;
 
-    int N, Q; cin >> N >> Q;
-    vector<vector<int>> tree(N+1);
-    for (int i = 2; i <= N; ++i) {
-        int p; cin >> p;
-        tree[p].emplace_back(i);
+void dfs(int x, int& clk) {
+    ts[x] = clk;
+    rmq[0][clk++] = x;
+    for (int y : g[x]) {
+        dep[y] = dep[x] + 1;
+        dfs(y, clk);
+        rmq[0][clk++] = x;
+    }
+    te[x] = clk;
+}
+
+int lca(int x, int y) {
+    int l = ts[x], r = ts[y]; if (l > r) swap(l, r);
+    int t = r - l + 1;
+    int i = MAXLOGN; while (~t & (1<<i)) --i;
+    int x1 = rmq[i][l], x2 = rmq[i][r - (1<<i) + 1];
+    return dep[x1] < dep[x2]? x1 : x2;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, q; cin >> n >> q;
+    for (int x = 2; x <= n; ++x) {
+        int px; cin >> px;
+        g[px].emplace_back(x);
+    }
+    dep[1] = 0;
+    int clk = 0; dfs(1, clk);
+
+    for (int i = 1; i <= MAXLOGN; ++i)
+    for (int j = 0; j + (1<<i>>1) < 2*n; ++j) {
+        int x1 = rmq[i - 1][j], x2 = rmq[i - 1][j + (1<<i>>1)];
+        rmq[i][j] = dep[x1] < dep[x2]? x1 : x2;
     }
 
-    HLD hld(tree);
-    SegTree segt(N);
+    SegTree segt(2*n);
 
-    while (Q--) {
-        int u, v, d; cin >> u >> v >> d;
-        int x = u, y = v;
-        bool r = true;
-
-        while (hld.ch_id[x] != hld.ch_id[y]) {
-            if (hld.ch_dep[x] > hld.ch_dep[y]) swap(x, y);
-            r = r && segt.query(hld.et[hld.ch_id[y]], hld.et[y]);
-            y = hld.par[hld.ch_id[y]];
+    while (q--) {
+        int x, y, op; cin >> x >> y >> op;
+        int r = segt.qry(ts[x]) + segt.qry(ts[y]) - 2*segt.qry(ts[lca(x, y)]);
+        cout << (r? "NO" : "YES") << '\n';
+        if (op) {
+            int z = r? y : x;
+            segt.upd(ts[z], +1);
+            segt.upd(te[z], -1);
         }
-        if (hld.ch_pos[x] > hld.ch_pos[y]) swap(x, y);
-        if (x != y)
-            r = r && segt.query(hld.et[x] + 1, hld.et[y]);
-        cout << (r? "YES" : "NO") << '\n';
-
-        if (d) segt.update(hld.et[r? u : v]);
     }
 
     return 0;
